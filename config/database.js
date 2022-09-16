af6815ca2dd9bb6a910d10c8f89e905633666926
eyosidesta <mamesmsdesta@gmail.com>
2022-09-16 16:54:30 +0300

    database configuration updated

--- config/database.js
+++ config/database.js
@@ -1,12 +1,13 @@
 const Sequelize = require('sequelize');
+const db_password = require('./db-constant');
 
-module.exports = new Sequelize('aipm', 'postgres', 'postgres', {
+module.exports = new Sequelize('aipm', 'postgres', `${db_password}`, {
     host: 'localhost',
     dialect: 'postgres',
 
     pool: {
         max: 5,
-        min: 0,
+        min: 0, 
         acquire: 30000,
         idle: 10000,
     },
